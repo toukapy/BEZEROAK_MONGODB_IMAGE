530f35ae9ffbf4ac0eb4c9425169e451d2f93963
Miren <mirensa2002@gmail.com>
2023-12-03 01:51:52 +0100

    First commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var ObjectID = require('mongodb').ObjectID;
 var router = express.Router();
 
-let users = [
-  {id: Date.now(), izena: "John", abizena: "Doe", email: "john@doe.com"},
-];
+const mongojs = require('mongojs');
+const db = mongojs("mongodb://127.0.0.1:27017/bezeroakdb", ['bezeroak']);
+
+let users = [];
+
+db.bezeroak.find(function(err, userdocs){
+  if(err){
+    console.log(err)
+  }else{
+    users = userdocs
+  }
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -20,19 +30,45 @@ router.get('/list', function(req, res, next) {
 
 router.post("/new", (req, res) => {
   users.push(req.body);
-  res.json(users);
+  db.bezeroak.insert(req.body, function(err, user){
+    if (err){
+      console.log(err)
+    }else{
+      console.log(user)
+      res.json(user);
+    }
+  })
 });
 
 router.delete("/delete/:id", (req, res) => {
-  users = users.filter(user => user.id != req.params.id);
-  res.json(users);
+  users = users.filter(user => user._id !== req.params.id);
+  db.bezeroak.remove({_id: mongojs.ObjectId(req.params.id)}, function (err, user){
+    if(err){
+      console.log(err)
+
+    }else{
+      console.log(req.params.id)
+      res.json(user);
+    }
+  })
+
+
 });
 
 router.put("/update/:id", (req, res) => {
-  let user = users.find(user => user.id == req.params.id);
+  let user = users.find(user => user._id === req.params.id);
   user.izena = req.body.izena;
   user.abizena = req.body.abizena;
   user.email = req.body.email;
+  db.bezeroak.update({_id: mongojs.ObjectId(req.params.id)},
+      {$set: {izena: req.body.izena, abizena:req.body.abizena, email:req.body.email}},
+      function(err, user){
+        if(err){
+          console.log(err)
+        }else{
+          console.log(user)
+        }
+      })
   res.json(users);
 })
 
