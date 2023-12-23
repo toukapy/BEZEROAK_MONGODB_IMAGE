9ad09773f2b1a33f54e29d55c23a34e702c69236
Touka <mirensa2002@gmail.com>
2023-12-23 02:29:41 +0100

    new data

--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
     formData.append("id", id);
 
     fetch("/users/new", {
-        method: "POST",
+        method: "PATCH",
         body: formData,
     })
       .then((response) => response.json())
