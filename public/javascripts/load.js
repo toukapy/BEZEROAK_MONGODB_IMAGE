9a23bb1fb5b5a65ecdcad67c7b0bed2c14f6d2da
Touka <mirensa2002@gmail.com>
2023-12-23 19:52:12 +0100

    new data

--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -1,8 +1,8 @@
 let updateUser = (id) => {
     let row = document.getElementById(id);
-    let izena = row.children[2].children[0].value;
-    let abizena = row.children[3].children[0].value;
-    let email = row.children[4].children[0].value;
+    let izena = row.children[1].children[0].value;
+    let abizena = row.children[2].children[0].value;
+    let email = row.children[3].children[0].value;
 
     let formData = new FormData();
 
@@ -10,18 +10,15 @@ let updateUser = (id) => {
     formData.append("abizena", abizena);
     formData.append("_id", id);
     formData.append("email", email);
-    formData.append("avatar", row.children[1].children[0].files[0]);
-
-    // Create a new img element
-    let img = document.createElement("img");
+    formData.append("avatar", row.children[4].children[0].files[0]);
 
     row.innerHTML = `
-        <th scope="row">${id}</th>
-        <td></td>
-        <td>${izena}</td>
-        <td>${abizena}</td>
-        <td>${email}</td>
-        <td><a onclick="deleteUser('${id}')">[x]</a> <a onclick="editUser('${id}')">[e]</a></td>
+    <th scope="row">${id}</th>
+    <td>${izena}</td>
+    <td>${abizena}</td>
+    <td>${email}</td>
+    <td><img id="avat" src="" width="50" height="50"/></td>
+    <td> <a onclick="deleteUser('${id}')">[x]</a> <a onclick="editUser('${id}')">[e]</a>  </td>
     `;
 
     fetch(`/users/update/${id}`, {
@@ -32,13 +29,9 @@ let updateUser = (id) => {
     .then(data => {
         console.log(data);
         console.log("Avatar: ", data.avatar);
-        // Set the attributes of the dynamically created img element
-        img.src = data.avatar;
-        img.width = 50;
-        img.height = 50;
-
-        // Append the img element to the td element in the updated HTML
-        row.children[1].appendChild(img);
+        let img = document.getElementById("avat");
+        img.src = `http://localhost:4000/uploads/${data.avatar}`;
+        img.removeAttribute("id");
     })
     .catch((error) => {
         console.error('Error:', error);
@@ -48,16 +41,15 @@ let updateUser = (id) => {
 
 let editUser = (id) => {
     let row = document.getElementById(id);
-    let izena = row.children[2].innerHTML;
-    let abizena = row.children[3].innerHTML;
-    let email = row.children[4].innerHTML;
+    let izena = row.children[1].innerHTML;
+    let abizena = row.children[2].innerHTML;
+    let email = row.children[3].innerHTML;
     row.innerHTML = `
     <th scope="row">${id}</th>
-    <td><input type="file" id="avatar2" ></td>
     <td><input type="text" id="izena" value="${izena}"></td>
     <td><input type="text" id="abizena" value="${abizena}"></td>
     <td><input type="text" id="email" value="${email}"></td>
-    
+    <td><input type="file" id="avatar" ></td>
     <td> <input type="button" onclick="updateUser('${id}')" value="Save"> </td>
     `;
 }
@@ -72,10 +64,10 @@ let insertUser = (user) => {
   newRow.setAttribute("id", user._id);
   newRow.innerHTML = `
                 <th scope="row">${user._id}</th>
-                <td><img src="${user.avatar}" width="50" height="50"></td>
                 <td>${user.izena}</td>
                 <td>${user.abizena}</td>
                 <td>${user.email}</td>
+                <td><img src="http://localhost:4000/uploads/${user.avatar}" width="50" height="50"></td>
                 <td><a onclick="deleteUser('${user._id}')">[x]</a> <a onclick="editUser('${user._id}')">[e]</a>  </td>
             `;
 };
